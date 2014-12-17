755eb5cc3a677866c4456695d18df0bdbf519f6c
William Casarin <bill@casarin.me>
2014-12-16 22:44:43 -0800

    more consistent types

--- rotate-bits.h
+++ rotate-bits.h
@@ -3,12 +3,10 @@
 #ifndef __ROTATE_DEFS_H
 #define __ROTATE_DEFS_H
 
-#define C64V(v) (unsigned long long)(v)
-
-#define U8V(v) ((unsigned char)(v) & 0xFFU)
-#define U16V(v) ((unsigned short)(v) & 0xFFFFU)
-#define U32V(v) ((unsigned long)(v) & 0xFFFFFFFFU)
-#define U64V(v) ((unsigned long long)(v) & 0xFFFFFFFFFFFFFFFFU)
+#define U8V(v) ((uint8_t)(v) & 0xFFU)
+#define U16V(v) ((uint16_t)(v) & 0xFFFFU)
+#define U32V(v) ((uint32_t)(v) & 0xFFFFFFFFU)
+#define U64V(v) ((uint64_t)(v) & 0xFFFFFFFFFFFFFFFFU)
 
 #ifdef _MSC_VER
 
@@ -23,11 +21,11 @@
 #else
 
 #define ROTL32(v, n) \
-  (U32V((unsigned long)(v) << (n)) | ((unsigned long)(v) >> (32 - (n))))
+  (U32V((uint32_t)(v) << (n)) | ((uint32_t)(v) >> (32 - (n))))
 
 // tests fail if we don't have this cast...
 #define ROTL64(v, n) \
-  (U64V((unsigned long long)(v) << (n)) | ((unsigned long long)(v) >> (64 - (n))))
+  (U64V((uint64_t)(v) << (n)) | ((uint64_t)(v) >> (64 - (n))))
 
 #define ROTR32(v, n) ROTL32(v, 32 - (n))
 #define ROTR64(v, n) ROTL64(v, 64 - (n))
@@ -35,10 +33,10 @@
 #endif
 
 #define ROTL8(v, n) \
-  (U8V((v) << (n)) | ((v) >> (8 - (n))))
+  (U8V((uint8_t)(v) << (n)) | ((uint8_t)(v) >> (8 - (n))))
 
 #define ROTL16(v, n) \
-  (U16V((v) << (n)) | ((v) >> (16 - (n))))
+  (U16V((uint16_t)(v) << (n)) | ((uint16_t)(v) >> (16 - (n))))
 
 #define ROTR8(v, n) ROTL8(v, 8 - (n))
 #define ROTR16(v, n) ROTL16(v, 16 - (n))
