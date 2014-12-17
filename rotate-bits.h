4e73a047acb6f9448c31b4a939488eb4721974dc
William Casarin <bill@casarin.me>
2014-12-16 22:49:11 -0800

    stdint

--- rotate-bits.h
+++ rotate-bits.h
@@ -3,11 +3,6 @@
 #ifndef __ROTATE_DEFS_H
 #define __ROTATE_DEFS_H
 
-#define U8V(v) ((uint8_t)(v) & 0xFFU)
-#define U16V(v) ((uint16_t)(v) & 0xFFFFU)
-#define U32V(v) ((uint32_t)(v) & 0xFFFFFFFFU)
-#define U64V(v) ((uint64_t)(v) & 0xFFFFFFFFFFFFFFFFU)
-
 #ifdef _MSC_VER
 
 #include <stdlib.h>
@@ -19,6 +14,13 @@
 #define ROTR64(v, n) _rotr64((v), (n))
 
 #else
+
+#include <stdint.h>
+
+#define U8V(v) ((uint8_t)(v) & 0xFFU)
+#define U16V(v) ((uint16_t)(v) & 0xFFFFU)
+#define U32V(v) ((uint32_t)(v) & 0xFFFFFFFFU)
+#define U64V(v) ((uint64_t)(v) & 0xFFFFFFFFFFFFFFFFU)
 
 #define ROTL32(v, n) \
   (U32V((uint32_t)(v) << (n)) | ((uint32_t)(v) >> (32 - (n))))
